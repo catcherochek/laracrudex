74fe3ba0162a8d1c87745b17bbf5a14125a81324
catcherochek <catcherochek@yandex.ru>
2019-05-10 03:43:25 +0300

    clean install

---
+++ 1.js
@@ -0,0 +1,11 @@
+var Person66875687 = /** @class */ (function () {
+    function Person66875687(name, age, salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    Person66875687.prototype.toString = function () {
+        return this.name + " (" + this.age + ")" + "(" + this.salary + ")";
+    };
+    return Person66875687;
+}());
